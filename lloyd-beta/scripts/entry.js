ba4188bf70073a3f093a04efbd2164c2ef8bc92e
Thomas McEvoy <tmcevoy213@gmail.com>
2017-05-25 01:38:19 -0400

    setup webpack

---
+++ lloyd-beta/scripts/entry.js
@@ -0,0 +1,20 @@
+import $ from 'jquery'
+import fullpage from 'fullpage.js'
+// import initHamburger from './hamburger'
+import adaptColorTo from './colorAdapter'
+
+$(() => {
+  // initHamburger()
+
+  $('#main').fullpage({
+    loopHorizontal: false,
+    onLeave: function(index, nextIndex, direction) {
+      adaptColorTo(nextIndex)
+    }
+  })
+  $('#logo-small').click(() => {
+    $.fn.fullpage.moveTo(1)
+    adaptColorTo(1)
+  })
+  $('.arrowDown').click(() => $.fn.fullpage.moveSectionDown())
+})
