1038e75870ff8e99b885850664f4700748b5ea1f
Thomas McEvoy <tmcevoy213@gmail.com>
2017-05-25 02:29:34 -0400

    fullpage

--- lloyd-beta/scripts/entry.js
+++ lloyd-beta/scripts/entry.js
@@ -1,7 +1,22 @@
 // import initHamburger from './initHamburger'
 import initFullpage from './initFullpage'
+import {debounce} from 'lodash'
 
 $(() => {
-  // initHamburger()
-  initFullpage()
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    // initHamburger()
+  }
+  else {
+    initFullpage()
+  }
+
+  $(window).on('resize', debounce(function() {
+    console.log($.fn.fullpage)
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      if ($.fn.fullpage) $.fn.fullpage.destroy()
+    }
+    else {
+      initFullpage()
+    }
+  }, 100));
 })
