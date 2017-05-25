557d533b62b4dea8057a791dff7afa2f6084f2c3
Thomas McEvoy <tmcevoy213@gmail.com>
2017-05-25 02:15:17 -0400

    restructure js

--- lloyd-beta/scripts/entry.js
+++ lloyd-beta/scripts/entry.js
@@ -1,20 +1,7 @@
-import $ from 'jquery'
-import fullpage from 'fullpage.js'
-// import initHamburger from './hamburger'
-import adaptColorTo from './colorAdapter'
+// import initHamburger from './initHamburger'
+import initFullpage from './initFullpage'
 
 $(() => {
   // initHamburger()
-
-  $('#fullpage').fullpage({
-    loopHorizontal: false,
-    onLeave: function(index, nextIndex, direction) {
-      adaptColorTo(nextIndex)
-    }
-  })
-  $('#logo-small').click(() => {
-    $.fn.fullpage.moveTo(1)
-    adaptColorTo(1)
-  })
-  $('.arrowDown').click(() => $.fn.fullpage.moveSectionDown())
+  initFullpage()
 })
