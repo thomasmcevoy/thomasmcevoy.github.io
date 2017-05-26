feaaee07b3471948fd8c7a3f43c00424919e401b
Thomas McEvoy <tmcevoy213@gmail.com>
2017-05-26 00:43:02 -0400

    hamburger menu

--- lloyd-beta/scripts/hamburger.js
+++ lloyd-beta/scripts/hamburger.js
@@ -3,21 +3,6 @@ import $ from 'jquery'
 const $hamburger = $('#hamburger')
 const $siteNav = $('#site-nav')
 
-let menuIsOpen = false
-
 export default function initHamburger() {
-  $hamburger.click(() => {
-    if (menuIsOpen) closeMenu()
-    else openMenu()
-  })
-}
-
-function closeMenu() {
-  $siteNav.css('left', '-9999px')
-  menuIsOpen = false
-}
-
-function openMenu() {
-  $siteNav.css('left', '0')
-  menuIsOpen = true
+  $hamburger.click(() => $siteNav.toggleClass('hidden'))
 }
