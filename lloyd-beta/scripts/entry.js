92e927ced702f897925b663ced083a09ee0c8e8b
Thomas McEvoy <tmcevoy213@gmail.com>
2017-05-26 01:28:43 -0400

    footerfix

--- lloyd-beta/scripts/entry.js
+++ lloyd-beta/scripts/entry.js
@@ -1,10 +1,8 @@
 import $ from 'jquery'
 import hamburgerInit from './hamburger'
 import fullpageInit from './fullpage'
-import footerFix from './footerFix'
 
 $(() => {
   hamburgerInit()
   fullpageInit()
-  footerFix()
 })
--- lloyd-beta/scripts/footerFix.js
+++ lloyd-beta/scripts/footerFix.js
@@ -1,20 +1,26 @@
-import $ from 'jquery'
-
-export default function footerFix() {
-  const windowHeight = window.innerHeight
-  const documentHeight = document.body.clientHeight
-  const $footer = $('footer')
-
-  if (windowHeight >= documentHeight) {
-    pin($footer)
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
-function pin($footer) {
-  $footer.css({
-    'position': 'fixed',
-    'bottom': '0',
-    'right': '0',
-    'left': '0'
-  })
+function footerFix() {
+  var windowHeight = window.innerHeight;
+  var documentHeight = document.body.clientHeight;
+  var footer = document.querySelector('footer');
+
+  if (windowHeight >= documentHeight) {
+    pin(footer);
+  }
 }
+
+function pin(footer) {
+  footer.style.position = 'fixed';
+  footer.style.bottom = '0';
+  footer.style.left = '0';
+  footer.style.right = '0';
+}
+
+ready(footerFix);
