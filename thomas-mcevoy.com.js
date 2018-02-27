ff00f0e7942583d02d43b1bf777e1017847d338e
Thomas McEvoy <tmcevoy213@gmail.com>
2018-02-27 02:29:12 -0500

    troubleshoot server

--- thomas-mcevoy.com.js
+++ thomas-mcevoy.com.js
@@ -1,5 +1,8 @@
+'use strict'
+
 const Koa = require('koa')
 const app = new Koa()
+const index = require('./index.html')
 
-app.use(ctx => ctx.body = index.html)
+app.use(ctx => ctx.body = index)
 app.listen(3000)
