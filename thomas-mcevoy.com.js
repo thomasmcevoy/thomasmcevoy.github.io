f8f45347a23add8e003ec28b2aec88f5dab2c6dd
Thomas McEvoy <tmcevoy213@gmail.com>
2018-02-27 02:19:13 -0500

    troubleshooting server

--- thomas-mcevoy.com.js
+++ thomas-mcevoy.com.js
@@ -1,5 +1,5 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(require('koa-static')('/home/thomas/thomasmcevoy.github.io/index.html'))
+app.use(require('koa-static')('./index.html'))
 app.listen(3000)
