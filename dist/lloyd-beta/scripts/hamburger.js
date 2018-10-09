bbc896f422532e04aa2236b9068da206fc5b7e25
Thomas McEvoy <tmcevoy213@gmail.com>
2018-10-09 05:29:57 -0400

    update packages

--- dist/lloyd-beta/scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery'
-
-const $hamburger = $('#hamburger')
-const $siteNav = $('#site-nav')
-
-export default function initHamburger() {
-  $hamburger.click(() => $siteNav.toggleClass('hidden'))
-}
